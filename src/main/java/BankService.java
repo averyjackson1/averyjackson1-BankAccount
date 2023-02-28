235fe56dbdcd3309c78123c8fb574d74dd0f9f8f
averyjackson1 <jackson.avery11@gmail.com>
2023-02-28 15:47:52 +0000

    done

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -17,29 +17,37 @@ public class BankService {
      */
     public BankService(){
         this.balance = 0;
+
     }
     /**
-     * TODO: implement functionality to increase the user's balance by amount.
+     * TO DO: implement functionality to increase the user's balance by amount.
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
+        for (amount = 0; amount >= 0; amount++ ){
+          balance = balance + amount;
+        }
 
     }
 
     /**
-     * TODO: implement functionality to decrease the user's balance by an amount.
+     * TO DO: implement functionality to decrease the user's balance by an amount.
      * If a withdrawl would result in the user having a negative balance, the withdrawl should not occur.
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
+        for(amount =0; amount >= 0; amount--){
+           balance = balance - amount;
 
-    }
+            }
+        }
+    
 
     /**
-     * TODO: return the balance.
+     * TO DO: return the balance.
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return balance;
     }
 }
